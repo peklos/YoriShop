e53b01c27d264269ac745bb231f994298bccf800
peklos <vladoserov727@mail.ru>
2025-06-21 21:22:34 +0300

    dev deploy

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,19 +1,26 @@
 import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("user", {
-  state: () => ({
-    cart: [],
-    favorite: [],
-  }),
+  state: () => {
+    const savedCart = localStorage.getItem("userCart");
+    const savedFav = localStorage.getItem("userFav");
+
+    return {
+      cart: savedCart ? JSON.parse(savedCart) : [],
+      favorite: savedFav ? JSON.parse(savedFav) : [],
+    };
+  },
   actions: {
     toggleMoveToCard(product) {
       if (!this.isCartInclude(product)) {
-        this.cart.push({...product, sum: 1});
+        this.cart.push({ ...product, sum: 1 });
       } else {
         const index = this.cart.findIndex((item) => item.id === product.id);
 
         this.cart.splice(index, 1);
       }
+
+      this.saveToLocalStorege("userCart", this.cart);
     },
 
     toggleMoveToFavorite(product) {
@@ -24,6 +31,12 @@ export const useUserStore = defineStore("user", {
 
         this.favorite.splice(index, 1);
       }
+
+      this.saveToLocalStorege("userFav", this.favorite);
+    },
+
+    saveToLocalStorege(key, data) {
+      localStorage.setItem(key, JSON.stringify(data));
     },
   },
   getters: {
@@ -33,6 +46,14 @@ export const useUserStore = defineStore("user", {
 
     isFavInclude: (state) => (product) => {
       return state.favorite.some((item) => item.id === product.id);
+    },
+
+    totalCartCounter() {
+      return this.cart.length;
+    },
+
+    totalFavCounter() {
+      return this.favorite.length;
     },
   },
 });
--- src/views/CartMain.vue
+++ src/views/CartMain.vue
@@ -21,31 +21,61 @@
         <!-- Заголовок -->
         <div class="flex justify-between items-center mb-8">
           <h1 class="text-3xl font-bold">КОРЗИНА</h1>
-          <span class="text-gray-400">3 товара</span>
+          <span class="text-gray-400"
+            >Всего товаров: {{ userStore.totalCartCounter }}</span
+          >
         </div>
 
         <!-- Основной блок корзины -->
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <!-- Список товаров -->
           <div class="lg:col-span-2 space-y-6">
-
-
-            <!-- Товар 1 -->
+            <!-- Блок для пустой корзины -->
+            <div
+              v-if="userStore.cart.length < 1"
+              class="col-span-full py-12 flex flex-col items-center justify-center space-y-4"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-16 w-16 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span class="text-2xl font-medium text-gray-500"
+                >Корзина пуста</span
+              >
+              <span class="text-gray-400"
+                >Ваши новые кроссовки ждут вас в каталоге</span
+              >
+            </div>
+            <!-- Товары -->
             <div
               class="bg-gray-900/50 rounded-lg p-4 flex flex-col sm:flex-row gap-4 border border-gray-800"
-              v-for="product in userStore.cart" :key="product"
+              v-for="product in userStore.cart"
+              :key="product.id"
             >
               <div class="w-full sm:w-32 h-32 flex-shrink-0">
                 <img
-                  :src="getImage('balenruner')"
-                  alt="Balenciaga Runner"
+                  :src="getImage(product.img)"
+                  :alt="product.img"
                   class="w-full h-full object-cover rounded"
                 />
               </div>
               <div class="flex-1">
                 <div class="flex justify-between">
-                  <h3 class="font-bold text-lg">BALENCIAGA RUNNER</h3>
-                  <button class="text-gray-400 hover:text-white">
+                  <h3 class="font-bold text-lg">{{ product.name }}</h3>
+                  <button
+                    class="text-gray-400 hover:text-white"
+                    @click="userStore.toggleMoveToCard(product)"
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       class="h-5 w-5"
@@ -60,18 +90,34 @@
                     </svg>
                   </button>
                 </div>
-                <p class="text-gray-400 mb-2">Размер: 42</p>
+                <p class="text-gray-400 mb-2">
+                  Размер: <span class="font-bold">{{ product.size }}</span>
+                </p>
                 <div class="flex items-center justify-between mt-auto">
                   <div class="flex items-center border border-gray-700 rounded">
-                    <button class="px-3 py-1 text-lg hover:bg-gray-800">
+                    <button
+                      class="px-3 py-1 text-lg hover:bg-gray-700 transition-all duration-100"
+                      @click="
+                        product.sum > 1
+                          ? (product.sum -= 1)
+                          : userStore.toggleMoveToCard(product)
+                      "
+                    >
                       -
                     </button>
-                    <span class="px-3">1</span>
-                    <button class="px-3 py-1 text-lg hover:bg-gray-800">
+                    <span class="px-3">{{ product.sum }}</span>
+                    <button
+                      class="px-3 py-1 text-lg hover:bg-gray-700 transition-all duration-100"
+                      @click="product.sum += 1"
+                    >
                       +
                     </button>
                   </div>
-                  <span class="font-bold text-lg">89 990 ₽</span>
+                  <span class="font-bold text-lg"
+                    >{{
+                      getCurrentPrice(product).toLocaleString("ru-RU")
+                    }}₽</span
+                  >
                 </div>
               </div>
             </div>
@@ -85,18 +131,25 @@
 
             <div class="space-y-4 mb-6">
               <div class="flex justify-between">
-                <span class="text-gray-400">Товары (3)</span>
-                <span>114 980 ₽</span>
+                <span class="text-gray-400"
+                  >Товары ({{ userStore.totalCartCounter }})</span
+                >
+                <span>{{ orderCost.toLocaleString("ru-RU") }} ₽</span>
               </div>
               <div class="flex justify-between">
                 <span class="text-gray-400">Доставка</span>
-                <span>Бесплатно</span>
+                <span v-if="orderCost >= 15000">Бесплатно</span>
+                <span v-if="orderCost < 15000"
+                  >{{ deliveryNoDiscount.toLocaleString("ru-RU") }} ₽</span
+                >
               </div>
               <div
                 class="border-t border-gray-700 pt-4 flex justify-between text-lg"
               >
                 <span>Итого</span>
-                <span class="font-bold">114 980 ₽</span>
+                <span class="font-bold"
+                  >{{ finalOrderCost.toLocaleString("ru-RU") }} ₽</span
+                >
               </div>
             </div>
 
@@ -117,59 +170,50 @@
         <div class="mt-16">
           <h2 class="text-2xl font-bold mb-6">ИЗБРАННОЕ</h2>
           <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            <!-- Товар 1 -->
             <div
-              class="bg-gray-900/50 rounded-lg p-4 border border-gray-800 group"
+              v-if="userStore.favorite.length < 1"
+              class="col-span-full py-12 flex flex-col items-center justify-center space-y-4"
             >
-              <div class="relative mb-3">
-                <img
-                  :src="getImage('givenchy')"
-                  alt="Givenchy Tee"
-                  class="w-full h-48 object-cover rounded"
-                />
-                <button
-                  class="absolute top-2 right-2 text-gray-400 hover:text-white"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </div>
-              <h3 class="font-bold">GIVENCHY TEE</h3>
-              <p class="text-gray-400 mb-2">Футболка</p>
-              <p class="font-bold">12 990 ₽</p>
-              <button
-                class="w-full mt-3 bg-gray-800 hover:bg-gray-700 py-2 text-sm rounded transition"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-16 w-16 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                В КОРЗИНУ
-              </button>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span class="text-2xl font-medium text-gray-500"
+                >Избранных нет</span
+              >
+              <span class="text-gray-400"
+                >Рекомендуем ознакомиться с новинками в каталоге</span
+              >
             </div>
-
-            <!-- Товар 2 -->
+            <!-- Товары -->
             <div
               class="bg-gray-900/50 rounded-lg p-4 border border-gray-800 group"
+              v-for="product in userStore.favorite"
+              :key="product.id"
             >
               <div class="relative mb-3">
                 <img
-                  :src="getImage('9060')"
-                  alt="New Balance"
+                  :src="getImage(product.img)"
+                  :alt="product.img"
                   class="w-full h-48 object-cover rounded"
                 />
                 <button
-                  class="absolute top-2 right-2 text-gray-400 hover:text-white"
+                  class="absolute top-2 right-2"
+                  @click="userStore.toggleMoveToFavorite(product)"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
+                    class="h-5 w-5 text-red-500 hover:text-red-600 transition-all duration-200"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
@@ -181,11 +225,16 @@
                   </svg>
                 </button>
               </div>
-              <h3 class="font-bold">NEW BALANCE 9060</h3>
-              <p class="text-gray-400 mb-2">Кроссовки</p>
-              <p class="font-bold">24 990 ₽</p>
+              <h3 class="font-bold">{{ product.name }}</h3>
+              <p class="text-gray-400 mb-2">
+                {{ product.category.toUpperCase() }}
+              </p>
+              <p class="font-bold">
+                {{ product.new_price.toLocaleString("ru-RU") }} ₽
+              </p>
               <button
                 class="w-full mt-3 bg-gray-800 hover:bg-gray-700 py-2 text-sm rounded transition"
+                @click="userStore.toggleMoveToCard(product)"
               >
                 В КОРЗИНУ
               </button>
@@ -194,28 +243,54 @@
         </div>
       </div>
     </div>
+    <email-subscribe />
   </div>
 </template>
 
 <script>
 import BannerOnTop from "@/components/BannerWithRunningLine.vue";
 import { useImagesStore } from "@/stores/imagesStore.js";
-import {useUserStore} from '@/stores/userStore.js'
+import { useUserStore } from "@/stores/userStore.js";
+import EmailSubscribe from "@/components/EmailSubscribe.vue";
 
 export default {
   name: "CartPage",
   data() {
     return {
       userStore: useUserStore(),
-    }
+      deliveryNoDiscount: 2000,
+    };
   },
   components: {
     BannerOnTop,
+    EmailSubscribe,
   },
   computed: {
     getImage() {
       const store = useImagesStore();
       return store.getImage;
+    },
+
+    orderCost() {
+      return this.userStore.cart.reduce((total, product) => {
+        return total + this.getCurrentPrice(product);
+      }, 0);
+    },
+
+    finalOrderCost() {
+      if (this.orderCost >= 15000) {
+        return this.orderCost;
+      } else if (this.orderCost == 0) {
+        return 0
+      } else {
+        return this.orderCost + 2000;
+      }
+    },
+  },
+
+  methods: {
+    getCurrentPrice(product) {
+      return product.new_price * product.sum;
     },
   },
 };
